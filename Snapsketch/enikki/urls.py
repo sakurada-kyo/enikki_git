880b9db8762ac3a8854a42d3fafd270c5be991b5
oonaka-hiroto <103411327+oonaka-hiroto@users.noreply.github.com>
2023-11-07 15:08:45 +0900

    経過途中２

--- Snapsketch/enikki/urls.py
+++ Snapsketch/enikki/urls.py
@@ -8,8 +8,5 @@ urlpatterns = [
     path('timeline/ajax_timeline/', views.ajax_timeline, name='ajax_timeline'),
     path('timeline/ajax_like/', views.ajax_like, name='ajax_like'),
     path('timeline/creategroup/', views.ajax_group, name='ajax_group'),
-    path('comment/', views.view_comment, name='comment'),
-    path('login/', views.view_index, name='login'),
-    path('account/', views.view_account, name='account'),
-    path('complete/', views.view_complete, name='complete'),
+
 ]
--- Snapsketch/enikki/views.py
+++ Snapsketch/enikki/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import get_object_or_404, render
 from .models import EnikkiModel,Like
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from django.views.generic import CreateView
-from . forms import UserCreateForm
 
 # タイムライン画面表示
 def view_timeline(request):
@@ -95,45 +92,3 @@ def ajax_group(request):
     groupName = request.POST.get('groupName')
     data = {}
     return JsonResponse(data)
-
-#アカウント作成
-class Create_account(CreateView):
-    def post(self, request, *args, **kwargs):
-        form = UserCreateForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            #フォームから'username'を読み取る
-            username = form.cleaned_data.get('username')
-            #フォームから'password1'を読み取る
-            password = form.cleaned_data.get('password1')
-            birthday = form.cleaned_data.get('birthday')
-            maleaddress = form.cleaned_data.get('maleaddress')
-            gender = form.cleaned_data.get('gender')
-
-            user = authenticate(username=username, password=password, birthday=birthday, maleaddress=maleaddress, gender=gender)
-            login(request, user)
-            return redirect('/')
-        return render(request, 'create.html', {'form': form,})
-
-    def get(self, request, *args, **kwargs):
-        form = UserCreateForm(request.POST)
-        return  render(request, 'create.html', {'form': form,})
-
-create_account = Create_account.as_view()
-
-#ログイン機能
-class Account_login(View):
-    def post(self, request, *arg, **kwargs):
-        form = LoginForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            user = User.objects.get(username=username)
-            login(request, user)
-            return redirect('/')
-        return render(request, 'login.html', {'form': form,})
-
-    def get(self, request, *args, **kwargs):
-        form = LoginForm(request.POST)
-        return render(request, 'login.html', {'form': form,})
-
-account_login = Account_login.as_view()
