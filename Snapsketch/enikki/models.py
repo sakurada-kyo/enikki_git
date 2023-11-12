635da49101f0a008f98be706c4df9581ffa67599
sakurada-kyo <103411513+sakurada-kyo@users.noreply.github.com>
2023-11-13 07:41:37 +0900

    models.py更新、canvas画面更新
    ・models.py ユーザマスタ作業中
    ・canvas.css 修正中

--- Snapsketch/enikki/models.py
+++ Snapsketch/enikki/models.py
@@ -1,84 +1,84 @@
+import uuid,re
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import validate_email,RegexValidator
 
-class UserManager(BaseUserManager):
+# アイコンパスを定義
+def directory_path(instance, filename):
+    cls_name = instance.__class__.__name__
+    
+    if cls_name == 'User':
+        return 'icon&draw/{0}/{1}'.format(instance.user_id, filename)
+    elif cls_name == 'PostMaster':
+        return 'icon&draw/{0}/{1}'.format(instance.user.user_id, filename)
+    else:
+        return 'group/{0}/{1}'.format(instance.groupname,filename)
 
-    def create_user(self, email, phone, gender, nickname, birthday, password=None):#ユーザーのフィールドを作成
-        if not email:
-            raise ValueError('Users must have an email address')
+# 電話番号チェック
+def validator_tel(val):
+    
+    pattern = r'[(]?\d{2,4}[-)]?\d{2,4}-\d{3,4}'
+    res = re.findall(pattern,val)
+    
+    return res
 
-        user = self.model(
-            email=self.normalize_email(email),
-            nickname=nickname,
-            birthday=birthday,
-            phone = phone,
-            gender = gender,
-            
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# 電話番号チェック
+def validator_birthday(val):
+    
+    pattern = r'\d{4}/\d{1,2}/\d{1,2}'
+    res = re.findall(pattern,val)
+    
+    return res
 
-    def create_superuser(self, email, nickname, birthday, password=None):#管理ユーザーのフィールドを作成
-        user = self.create_user(
-            email,
-            password=password,
-            nickname=nickname,
-            birthday=birthday,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser):
-    user_id = models.CharField(primary_key=True,max_length=255)
-    username = models.CharField(max_length=255)
-    username_kana = models.CharField(max_length=255)
-    email = models.EmailField(
-        verbose_name='email address',
+# ユーザーマスタ
+class User(AbstractUser):
+    
+    username_validator = UnicodeUsernameValidator()
+    
+    # 不要なフィールドはNoneにすることができる
+    first_name = None
+    last_name = None
+    date_joined = None
+    groups = None
+    
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(
         max_length=255,
         unique=True,
+        validators=[username_validator]
     )
-    nickname = models.CharField(max_length=255)
+    email = models.CharField(max_length=254, unique=True,validators=validate_email)
+    tel = models.CharField(
+            max_length=15, 
+            unique=True,
+            validators=validator_tel
+        )
     gender = models.CharField(max_length=1)
-    birthday = models.DateField()
-    icon_id = models.CharField(max_length=255)
-    password = models.CharField(unique=True,max_length=255)
+    birthday = models.CharField(max_length=11,validators=validator_birthday)
+    icon_path = models.ImageField(upload_to=directory_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    # USERNAME_FIELD = "username"
 
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['nickname', 'birthday']
+    class Meta:
+        ordering = ['username']
 
     def __str__(self):
-        return self.email
+        return self.username
 
-    def has_perm(self, perm, obj=None):#Trueを返して、権限があることを知らせる
-        return True
-
-    def has_module_perms(self, app_label):#Trueを返して、アプリ（App）のモデル（Model）へ接続できるようにする
-        return True
-
-    @property
-    def is_staff(self):#Trueの場合、Django管理サイトにログインできる
-        return self.is_admin
-
-class IconMaster(models.Model):
-    icon_id = models.CharField(primary_key=True,max_length=255)
-    icon_path = models.CharField(unique=True,null=False,max_length=255)
-
+# グループマスタ
 class GroupMaster(models.Model):
     group_id = models.CharField(primary_key=True,max_length=255)
     groupname = models.CharField(max_length=255,null=False)
-    icon_id = models.ForeignKey(IconMaster,on_delete=models.CASCADE)
+    group_icon_path = models.ImageField(upload_to=directory_path)
 
+# 投稿マスタ
 class PostMaster(models.Model):
     post_id = models.CharField(primary_key=True,max_length=255)
-    sketch_path = models.CharField(max_length=255)
+    sketch_path = models.ImageField(upload_to=directory_path)
     diary = models.CharField(blank=True,null=True,max_length=255)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     likeCount = models.IntegerField(default=0,null=True)
@@ -86,63 +86,46 @@ class PostMaster(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True,null=True)
+    
+    
 
-class FriendTable(models.Model):
-    user_id = models.CharField(null=True,max_length=255)
-    friend_id = models.CharField(null=True,max_length=255)
-
-    class Meta:
-       constraints = [
-           # user_idとfriend_idでユニーク制約
-           models.UniqueConstraint(fields=['user_id', 'friend_id'], name='unique_stock')
-       ]
-
+# ユーザーグループテーブル
 class UserGroupTable(models.Model):
-    user_id = models.CharField(max_length=255)
-    group_id = models.CharField(max_length=255)
-    
-    class Meta:
-       constraints = [
-           # user_idとgroup_idでユニーク制約
-           models.UniqueConstraint(fields=['user_id', 'group_id'], name='unique_UserGroup')
-       ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user')
+    group = models.ForeignKey(GroupMaster, on_delete=models.CASCADE,related_name='group')
 
-class FollowersTable(models.Model):
-    user_id = models.CharField(max_length=255)
-    friend_id = models.CharField(max_length=255)
+# フォロワーテーブル
+class Follower(models.Model):
+    follower  = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower_user')
+    followee  = models.ForeignKey(User,  on_delete=models.CASCADE, related_name='followee_user')
     
-    class Meta:
-       constraints = [
-           # group_idとimage_idでユニーク制約
-           models.UniqueConstraint(fields=['group_id', 'image_id'], name='unique_Fllowers')
-       ]
+    def __str__(self):
+        return "{} : {}".format(self.follower.username, self.followee.username)
 
+# いいねテーブル
 class LikeTable(models.Model):
-    user_id = models.CharField(max_length=255)
-    post_id = models.CharField(max_length=255)
-    
-    class Meta:
-       constraints = [
-           # user_idとpost_idでユニーク制約
-           models.UniqueConstraint(fields=['user_id', 'post_id'], name='unique_Like')
-       ]
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user')
+    post_id = models.ForeignKey(PostMaster, on_delete=models.CASCADE,related_name='post')
 
+# コメントマスタ
 class CommentMaster(models.Model):
     comment_id = models.CharField(primary_key=True,max_length=255)
-    user_id = models.CharField(max_length=255)
-    post_id = models.CharField(max_length=255)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user')
+    post_id = models.ForeignKey(PostMaster, on_delete=models.CASCADE,related_name='post')
     comment = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True,null=True)
 
+#グループ投稿テーブル
 class GroupPostTable(models.Model):
-    group_id = models.CharField(max_length=255)
-    post_id = models.CharField(max_length=255)
+    group_id = models.ForeignKey(GroupMaster, on_delete=models.CASCADE,related_name='group')
+    post_id = models.ForeignKey(PostMaster, on_delete=models.CASCADE,related_name='post')
     page = models.IntegerField(max_length=255,default=1)
 
     class Meta:
-       constraints = [
+        constraints = [
            # group_idとpost_idでユニーク制約
            models.UniqueConstraint(fields=['group_id', 'post_id'], name='unique_GroupPost')
-       ]
+        ]
+        
