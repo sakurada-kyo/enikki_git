d122c494272972ac43fa601643f77fd6f3062e48
ueno-saki <103411354+ueno-saki@users.noreply.github.com>
2023-11-16 14:06:08 +0900

    ユーザー検索画面更新
    ・usersearch更新

--- Snapsketch/enikki/urls.py
+++ Snapsketch/enikki/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name = 'enikki'
 
 urlpatterns = [
-    path('timeline/', views.view_timeline, name='timeline'),
-    path('timeline/ajax_timeline/', views.ajax_timeline, name='ajax_timeline'),
-    path('friend/', views.view_friend,name='friend'),
-    path('mypage/', views.view_mypage,name='mypage'),
-    path('comment/', views.view_comment,name='comment'),
+    path('calendar/', views.view_calendar, name='calendar'),
+    path('usersearch/', views.view_usersearch, name='usersearch'),
 ]
--- Snapsketch/enikki/views.py
+++ Snapsketch/enikki/views.py
@@ -1,59 +1,9 @@
-import json
-from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
-from .models import EnikkiModel,Friend,MyPage,Comment
 
-#タイムライン画面表示
-def view_timeline(request):
-    model = EnikkiModel.objects.all()
-    # page = request.GET.get("page")
-    print(model)
-    context = {
-        "enikki_list":model,
-        "current_path":request.path
-    }
-    return render(request,'timeline.html',context)
+def view_calendar(request):
+    
+    return render(request,'calendar.html')
 
-#タイムラインのajax
-def ajax_timeline(request):
-
-    groupName = str(request.POST.get('groupName'))
-    page = int(request.POST.get('page'))
-
-    # グループ名+page番号より上の行を持ってくる
-    data = {
-        "userIconPath":{},
-        "userName":{},
-        "drawPath":{},
-        "diary":{},
-        "groupName":{},
-        "page":{}
-    }
-    return JsonResponse(data)
-
-    #友人一覧
-def view_friend(request):
-    # friend = Friend.objects.all().filter(userId=request.userId)
-    friend = Friend.objects.all()
-    context = {
-        'friends':friend
-    }
-    return render(request,'friend.html',context)
-
-    #マイページ
-def view_mypage(request):
-    mypage = MyPage.objects.get(user_id='20010101')
-    print(mypage)
-    context = {
-        'mypage':mypage
-    }
-    return render(request,'myPage.html',context)
-
-    #コメント
-def view_comment(request):
-    comment = Comment.objects.all()
-    userName = str(request.POST.get('user_name'))
-    context = {
-        'comment':comment
-    }
-    return render(request,'comment.html',context)
+def view_usersearch(request):
+    
+    return render(request,'usersearch.html')
