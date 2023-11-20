3595f6d8d96215a88beb8faf52672bf0266ec464
sakurada-kyo <x20u022@chiba-fjb.ac.jp>
2023-11-20 10:06:17 +0900

    urls.py更新

--- Snapsketch/enikki/urls.py
+++ Snapsketch/enikki/urls.py
@@ -4,11 +4,13 @@ from . import views
 app_name = 'enikki'
 
 urlpatterns = [
-    # path('timeline/', views.view_timeline, name='timeline'),
-    # path('timeline/ajax_timeline/', views.ajax_timeline, name='ajax_timeline'),
-    # path('timeline/ajax_like/', views.ajax_like, name='ajax_like'),
-    # path('timeline/creategroup/', views.ajax_group, name='ajax_group'),
-    # path('timeline/comment/', views.view_comment, name='comment'),
+    path('timeline/', views.view_timeline, name='timeline'),
+    path('timeline/ajax_timeline/', views.ajax_timeline, name='ajax_timeline'),
+    path('timeline/ajax_like/', views.ajax_like, name='ajax_like'),
+    path('timeline/creategroup/', views.ajax_group, name='ajax_group'),
+    path('timeline/comment/', views.view_comment, name='comment'),
     path('canvas/', views.CanvasView.as_view(), name='canvas'),
     path('create/', views.CreateView.as_view(), name='create'),
+    # path('mypage/',, name='mypage'),
+    # path('friend/',, name='mypage'),
 ]
