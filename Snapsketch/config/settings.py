da7d336629e2c972b6cc34ed3606988386639ee8
sakurada-kyo <x20u022@chiba-fjb.ac.jp>
2023-11-20 10:24:30 +0900

    login_app更新

--- Snapsketch/config/settings.py
+++ Snapsketch/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'enikki.apps.EnikkiConfig',
     'login_app.apps.LoginAppConfig',
-    'bootstrap4',
+    # 'bootstrap4',
 ]
 
 MIDDLEWARE = [
