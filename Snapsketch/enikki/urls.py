17a00e7ea46c24000ed9ee58b136235d3eaf960e
sato-sota <x20u024@chiba-fjb.ac.jp>
2023-11-20 11:51:34 +0900

    コメント更新

--- Snapsketch/enikki/urls.py
+++ Snapsketch/enikki/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('timeline/ajax_timeline/', views.ajax_timeline, name='ajax_timeline'),
     # path('timeline/ajax_like/', views.ajax_like, name='ajax_like'),
     # path('timeline/creategroup/', views.ajax_group, name='ajax_group'),
-    # path('timeline/comment/', views.view_comment, name='comment'),
+    path('timeline/comment/', views.view_comment, name='comment'),
     path('canvas/', views.CanvasView.as_view(), name='canvas'),
     path('create/', views.CreateView.as_view(), name='create'),
 ]
