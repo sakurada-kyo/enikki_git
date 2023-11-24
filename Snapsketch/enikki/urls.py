c3793180639fbc93295a06fe9cf8e50384ceb6af
maeno-rukiya <x20u037@chiba-fjb.ac.jp>
2023-11-24 10:08:55 +0900

    更新

--- Snapsketch/enikki/urls.py
+++ Snapsketch/enikki/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "enikki"
 
 urlpatterns = [
-    path('timeline/', views.view_timeline, name='timeline'),
+    path('timeline/', views.TimelineView.as_view(), name='timeline'),
     # path('timeline/ajax_timeline/', views.ajax_timeline, name='ajax_timeline'),
     # path('timeline/ajax_like/', views.ajax_like, name='ajax_like'),
     # path('timeline/creategroup/', views.ajax_group, name='ajax_group'),
