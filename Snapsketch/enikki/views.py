96d484147efd52fdc8e37a80b5fce5541a354de3
sato-sota <x20u024@chiba-fjb.ac.jp>
2023-12-01 09:34:02 +0900

    朝9:33分上野、前野不在

--- Snapsketch/enikki/views.py
+++ Snapsketch/enikki/views.py
@@ -446,25 +446,25 @@ def view_accountView(request):
 
 #フォローリクエスト機能
 #ユーザー検索機能
-class RequestView(TemplateView):
+# class RequestView(TemplateView):
 
-    template_name = 'request.html'
+#     template_name = 'request.html'
     
-    #検索されたuserIdを取得する
-    frId = request.POST["frId"]
+#     #検索されたuserIdを取得する
+#     frId = request.POST["frId"]
 
-    try:
-        # 指定した日付とログインユーザーに基づいてレコードを抽出
-        post = get_object_or_404(PostMaster, user_id=userId, created_at=date)
-        #データが存在するか調べる
-        friend = user.objects.filter(user_id__exact=frId)
-        #Requestmodelにデータを追加する
+#     try:
+#         # 指定した日付とログインユーザーに基づいてレコードを抽出
+#         post = get_object_or_404(PostMaster, user_id=userId, created_at=date)
+#         #データが存在するか調べる
+#         friend = user.objects.filter(user_id__exact=frId)
+#         #Requestmodelにデータを追加する
         
-        except Http404:
-            PostMaster.objects.create(diary=diary,user=userId)
-            return 
+#         except Http404:
+#             PostMaster.objects.create(diary=diary,user=userId)
+#             return 
     
-    user = get_user_model()
+#     user = get_user_model()
         #デフォルトのuserモデルを参照して情報を引っ張る
 # マイページ機能
 
