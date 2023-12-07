ac76a438e8fb9d338737655bcd1f887642628093
sakurada-kyo <x20u022@chiba-fjb.ac.jp>
2023-12-07 13:44:41 +0900

    更新

--- Snapsketch/login_app/forms.py
+++ Snapsketch/login_app/forms.py
@@ -1,13 +1,37 @@
+# forms.py
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
-class SignupForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1','password2']
+class CustomUserCreationForm(UserCreationForm):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
 
-class LoginForm(AuthenticationForm):
-    pass
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
+    birthday = forms.DateField(
+        widget=forms.SelectDateWidget(years=range(1920, 2101), attrs={'class': 'form-select'})
+    )
 
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'ユーザー名'
+        self.fields['email'].label = 'メールアドレス'
+        self.fields['password1'].label = 'パスワード'
+        self.fields['password2'].label = 'パスワード確認'
+        self.fields['user_icon_path'].label = 'アイコン画像'
+        self.fields['tel'].label = '電話番号'
+        self.fields['gender'].label = '性別'
+        self.fields['gender'].choices = [
+            ('M', '男性'),
+            ('F', '女性'),
+            ('O', 'その他'),
+        ]
+        self.fields['birthday'].label = '誕生日'
+        
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'user_icon_path', 'tel', 'gender', 'birthday')
--- Snapsketch/login_app/models.py
+++ Snapsketch/login_app/models.py
@@ -1,1 +1,33 @@
+# models.py
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+import uuid
+
+class CustomUser(AbstractUser):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    user_icon_path = models.ImageField(upload_to='user_icons/')
+    tel = models.CharField(max_length=15,null=True,blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    birthday = models.DateField(null=True,blank=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True,null=True)
+    
+    first_name = None  # First nameを削除
+    last_name = None  # Last nameを削除
+    date_joined = None  # Date joinedを削除
+
+
+    def __str__(self):
+        return self.username
