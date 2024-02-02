3b5c57bfb45b3918b8985f642bb1dd81a8c85c2a
ueno-saki <103411354+ueno-saki@users.noreply.github.com>
2024-02-02 13:45:09 +0900

    更新：上野

---
+++ Snapsketch/static/js/base3.js
@@ -0,0 +1,5 @@
+$(".openbtn4").click(function () {
+    $(this).toggleClass('active');
+    var hamburgerMenu = document.querySelector('.hamburger-menu');
+    hamburgerMenu.style.display = (hamburgerMenu.style.display === 'none' || hamburgerMenu.style.display === '') ? 'block' : 'none';
+});
