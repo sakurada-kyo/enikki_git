4b2bc7ec2567629a8c141c007249d1950ca22b83
ueno-saki <103411354+ueno-saki@users.noreply.github.com>
2024-02-07 16:09:46 +0900

    更新：上野

--- Snapsketch/static/js/login.js
+++ Snapsketch/static/js/login.js
@@ -57,6 +57,19 @@
 // //     })
 // // })
 
+// eye-iconのclickクリックイベント
+$("#eye-icon").on("click", () => {
+    // eye-iconのclass切り替え
+    $("#eye-icon").toggleClass("fa-eye-slash fa-eye");
+
+    // inputのtype切り替え
+    if ($("#id_password").attr("type") == "password") {
+        $("#id_password").attr("type", "text");
+    } else {
+        $("#id_password").attr("type", "password");
+    }
+});
+
 
 
 
