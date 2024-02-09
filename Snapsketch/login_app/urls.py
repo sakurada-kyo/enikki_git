7dc4f1d9f0136b2db20cbb12a9ddf2b4a295efe4
sakurada-kyo <x20u022@chiba-fjb.ac.jp>
2024-02-09 11:12:55 +0900

    update urls
    ・サインアウト　追加

--- Snapsketch/login_app/urls.py
+++ Snapsketch/login_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # 他のURLパターンとマージする場合は適切に調整する
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout'),
     path('complete/', views.complete, name='complete'),
     path('password_reset/', views.PasswordReset.as_view(), name='password_reset'), #追加
     path('password_reset/done/', views.PasswordResetDone.as_view(), name='password_reset_done'), #追加
