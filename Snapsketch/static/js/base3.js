20c069fca91ddeb6c1ccdfcf20b64132b77facff
sakurada-kyo <x20u022@chiba-fjb.ac.jp>
2024-02-09 11:12:31 +0900

    update base

--- Snapsketch/static/js/base3.js
+++ Snapsketch/static/js/base3.js
@@ -1,5 +1,5 @@
-$(".openbtn4").click(function () {
-    $(this).toggleClass('active');
-    var hamburgerMenu = document.querySelector('.hamburger-menu');
-    hamburgerMenu.style.display = (hamburgerMenu.style.display === 'none' || hamburgerMenu.style.display === '') ? 'block' : 'none';
+$(function () {
+    $("#logout").on('click',() => {
+        window.location.href = "/login_app/signout";
+    })
 })
