dbbd530a8be5ba59a05c928247437a26e57b34d4
sakurada-kyo <x20u022@chiba-fjb.ac.jp>
2024-02-09 14:20:08 +0900

    update react

--- Snapsketch/enikki/src/components/GroupCreatePopup.js
+++ Snapsketch/enikki/src/components/GroupCreatePopup.js
@@ -86,7 +86,7 @@ const GroupCreatePopup = (props) => {
                         </label>
                         
                     </div>
-                    <button id='createBtn' onClick={fetchGroupCreate} disabled>作成</button>
+                    <button id='createBtn' onClick={fetchGroupCreate}>作成</button>
                 </div>
             </div>
         </div>
